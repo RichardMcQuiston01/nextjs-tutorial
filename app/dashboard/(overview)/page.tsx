ebad1dac68fd59bae99b538b21918961b31b0d4d
Richard McQuiston <richard.m@engagelearningsystems.com>
2024-04-20 18:12:58 -0400

    Trying to fix bug from tutorial preventing deployment...

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,5 +1,4 @@
-import { fetchCardData, fetchLatestInvoices } from '@/app/lib/data';
-import CardWrapper, { Card } from "@/app/ui/dashboard/cards";
+import CardWrapper from "@/app/ui/dashboard/cards";
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import { lusitana } from '@/app/ui/fonts';
@@ -13,17 +12,23 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Suspense fallback={<CardsSkeleton />}>
-          <CardWrapper />
-        </Suspense>
+        <>
+          <Suspense fallback={ <CardsSkeleton/> }>
+            <CardWrapper/>
+          </Suspense>
+        </>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <Suspense fallback={ <RevenueChartSkeleton/> }>
-          <RevenueChart/>
-        </Suspense>
-        <Suspense fallback={ <LatestInvoicesSkeleton/> }>
-          <LatestInvoices/>
-        </Suspense>
+        <>
+          <Suspense fallback={<RevenueChartSkeleton />}>
+            <RevenueChart />
+          </Suspense>
+        </>
+        <>
+          <Suspense fallback={ <LatestInvoicesSkeleton/> }>
+            <LatestInvoices/>
+          </Suspense>
+        </>
       </div>
     </main>
   );
